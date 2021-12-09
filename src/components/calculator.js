45521401cb90c1eec978d6288b6ec19c4d7ef1fb
rolandm99 <manfulmweze99@gmail.com>
2021-12-09 21:24:57 +0100

    Update components directory for handle events

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,36 +1,61 @@
 import React, { Component } from 'react';
+import InputButton from './input';
+import calculate from '../logic/calculate';
 import '../index.css';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
   }
 
   render() {
+    const handleCursor = (clickName) => {
+      const name = clickName.target.innerText;
+      const { total, next, operation } = calculate(this.state, name);
+      this.setState({ total, next, operation });
+    };
+    const { total, next, operation } = this.state;
+
+    const display = () => {
+      if (next) {
+        return next;
+      }
+      if (operation) {
+        return operation;
+      }
+      if (total) {
+        return total;
+      }
+      return 0;
+    };
     return (
       <div className="main-container">
-        <div className="display">{0}</div>
+        <div className="display">{display()}</div>
         <div className="calculator-btn">
-          <input type="button" value="AC" className="c-btn" />
-          <input type="button" value="+/-" className="c-btn" />
-          <input type="button" value="%" className="c-btn" />
-          <input type="button" value="&divide;" className="c-btn orange" />
-          <input type="button" value="7" className="c-btn" />
-          <input type="button" value="8" className="c-btn" />
-          <input type="button" value="9" className="c-btn" />
-          <input type="button" value="X" className="c-btn orange" />
-          <input type="button" value="4" className="c-btn" />
-          <input type="button" value="5" className="c-btn" />
-          <input type="button" value="6" className="c-btn" />
-          <input type="button" value="-" className="c-btn orange" />
-          <input type="button" value="1" className="c-btn" />
-          <input type="button" value="2" className="c-btn" />
-          <input type="button" value="3" className="c-btn" />
-          <input type="button" value="+" className="c-btn orange" />
-          <input type="button" value="0" className="c-btn double" />
-          <input type="button" value="." className="c-btn" />
-          <input type="button" value="3" className="c-btn orange" />
+          <InputButton value="AC" className="c-btn" click={handleCursor} />
+          <InputButton value="+/-" className="c-btn" click={handleCursor} />
+          <InputButton value="%" className="c-btn" click={handleCursor} />
+          <InputButton value="÷" className="c-btn orange" click={handleCursor} />
+          <InputButton value="7" className="c-btn" click={handleCursor} />
+          <InputButton value="8" className="c-btn" click={handleCursor} />
+          <InputButton value="9" className="c-btn" click={handleCursor} />
+          <InputButton value="X" className="c-btn orange" click={handleCursor} />
+          <InputButton value="4" className="c-btn" click={handleCursor} />
+          <InputButton value="5" className="c-btn" click={handleCursor} />
+          <InputButton value="6" className="c-btn" click={handleCursor} />
+          <InputButton value="-" className="c-btn orange" click={handleCursor} />
+          <InputButton value="1" className="c-btn" click={handleCursor} />
+          <InputButton value="2" className="c-btn" click={handleCursor} />
+          <InputButton value="3" className="c-btn" click={handleCursor} />
+          <InputButton value="+" className="c-btn orange" click={handleCursor} />
+          <InputButton value="0" className="c-btn double" click={handleCursor} />
+          <InputButton value="." className="c-btn" click={handleCursor} />
+          <InputButton value="=" className="c-btn orange" click={handleCursor} />
         </div>
       </div>
     );
