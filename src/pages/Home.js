8a578649bc264c3d01d6eab3047bc09587a9b3ad
rolandm99 <manfulmweze99@gmail.com>
2021-12-24 06:15:34 +0100

    Update Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,21 @@ import React from 'react';
 
 const Home = () => (
   <>
-    <div className="home">
-      <h1>Welcome to Our Page!</h1>
-      <p>
-        Mathematics are the result of mysterious powers which no one understands,
-        and which the unconscious recognition of beauty must play an important part.
-        Out of an infinity of designs a mathematician chooses one pattern for
-        beauty’s sake and pulls it down to earth.
-      </p>
-      <p>
-        It is impossible to be a mathematician without being a poet in soul&#128525;
-      </p>
-      <br />
-      <p>Let&apos;s have fun with Math-Magician&#128513;&#128517;</p>
+    <div className="my-home">
+      <div className="home">
+        <h1>Welcome to Our Page!</h1>
+        <p>
+          Mathematics are the result of mysterious powers which no one understands,
+          and which the unconscious recognition of beauty must play an important part.
+          Out of an infinity of designs a mathematician chooses one pattern for
+          beauty’s sake and pulls it down to earth.
+        </p>
+        <p>
+          It is impossible to be a mathematician without being a poet in soul&#128525;
+        </p>
+        <br />
+        <p>Let&apos;s have fun with Math-Magician&#128513;&#128517;</p>
+      </div>
     </div>
   </>
 );
